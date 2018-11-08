9a88b5e0e9bc09c8c24026eb1b01b128d307a693
victor smart <vic2oke@gmail.com>
2018-11-08 01:25:54 +0100

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,3 @@
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
